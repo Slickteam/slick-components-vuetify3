d62cec19722ab8c2570633e5f877b491131c377f
Ronan MOREL <ronan.morel@slickteam.fr>
2024-07-18 15:45:47 +0200

    fix: options settings of vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "@mdi/font/css/materialdesignicons.min.css";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import { VNumberInput } from "vuetify/labs/VNumberInput";
 
 import {
   MoleculeIconButton,
@@ -17,30 +16,27 @@ const SlickComponentLibrary = {
 
 const VuetifyLibrary = {
   install(app, options) {
-    const chooseTheme = options?.theme ?? {
-      themes: {
-        light: {
-          dark: false,
-          colors: {
-            background: "#f5f5f9",
-            surface: "#fff",
-            primary: "#f18900",
-            secondary: "#0068f1",
-            success: "#00c853",
-            warning: "#ffc107",
-            error: "#f44336",
-            info: "#03c9d7",
+    const defaultOptions = options ?? {
+      theme: {
+        themes: {
+          light: {
+            dark: false,
+            colors: {
+              background: "#f5f5f9",
+              surface: "#fff",
+              primary: "#f18900",
+              secondary: "#0068f1",
+              success: "#00c853",
+              warning: "#ffc107",
+              error: "#f44336",
+              info: "#03c9d7",
+            },
           },
         },
       },
     };
 
-    const vuetify = createVuetify({
-      components: {
-        VNumberInput,
-      },
-      theme: chooseTheme,
-    });
+    const vuetify = createVuetify(defaultOptions);
     app.use(vuetify);
   },
 };
