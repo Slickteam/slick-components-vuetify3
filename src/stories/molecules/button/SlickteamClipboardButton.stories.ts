f0edcbe2de7c9cc2ec8daaee4515250acbea1556
Ronan MOREL <ronan.morel@slickteam.fr>
2026-02-02 13:59:32 +0100

    chore: update storybook

--- src/stories/molecules/button/SlickteamClipboardButton.stories.ts
+++ src/stories/molecules/button/SlickteamClipboardButton.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
-import { fn } from 'storybook/internal/test';
+import { fn } from 'storybook/test';
 
 import SlickteamClipboardButton from '../../../components/molecules/button/SlickteamClipboardButton.vue';
 
